6d099ac27c0222285c411578cc1356d0d496dabf
BBJ197 <beamlakbekele197@gmail.com>
2026-03-03 13:25:51 +0300

    Step 2 - 05_joinStrings has been DONE

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -7,10 +7,17 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
+const { lastName, thisYear } = require("./joinStrings-example");
+
 // Add your code right below, good luck!
+let firstName = "Carlos";
+// const lastName = "Stevenson";
+// let thisYear = 1965;  // would have used this 2 lines of codes if there weren't the first code 
+let birthYear = 1947;
 
+let greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " Years old.";
 
-
+console.log (greeting)
 
 
 // Do not change this
