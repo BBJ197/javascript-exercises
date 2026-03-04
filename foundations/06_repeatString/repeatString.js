0047e297320486028641ca7abbf8b8dc6cee0af7
BBJ197 <beamlakbekele197@gmail.com>
2026-03-04 09:58:09 +0300

    06-repeatString has been complited

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (String, numberOfTimes) {
+    let repeatedString = ""
+    for (let i = 1; i <= numberOfTimes; i++){
+        repeatedString = repeatedString + String
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
