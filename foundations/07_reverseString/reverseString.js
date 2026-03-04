6510dafa5e4845a2ee3512ee4b0bed3707f05688
BBJ197 <beamlakbekele197@gmail.com>
2026-03-04 10:06:37 +0300

    07_reverseString has been finished

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(String) {
+    let reversedString = ""
+    for (let i = 0; i < String.length; i++){
+        let j = String.length - 1 - i
+        reversedString += String[j]
+    }
+    return reversedString
 };
 
 // Do not edit below this line
