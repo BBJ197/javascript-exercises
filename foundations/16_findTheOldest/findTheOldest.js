a529a342468a48f12b1e0ebfeb395d812d314081
BBJ197 <beamlakbekele197@gmail.com>
2026-03-06 13:04:26 +0300

    16_findTheOldest is done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort((a, b) =>  (b.yearOfDeath - b.yearOfBirth) -(a.yearOfDeath - a.yearOfBirth) )
+    return(oldest[0])
 };
 
 // Do not edit below this line
