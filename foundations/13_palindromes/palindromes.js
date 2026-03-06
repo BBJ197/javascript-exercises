6176bc13031a1e11639a9d4dea9cfd109cfc1ebc
BBJ197 <beamlakbekele197@gmail.com>
2026-03-06 12:35:11 +0300

    13_palindromes is done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let reverced = ""
+    for (let i=0; i<str.length; i++){
+        reverced = reverced + str[str.length - i - 1]
+    }
+    if (str === reverced){
+        return true
+    }
+    else {
+       return false
+    }
 };
 
 // Do not edit below this line
