3683dc9cc478b7314405edc0fced79980d7fd912
BBJ197 <beamlakbekele197@gmail.com>
2026-03-06 12:22:19 +0300

    12_calculator is done

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return(num1 + num2)
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return(num1 - num2)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	for (let i in array){
+    total = total + array[i]
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplicity = 0;
+	for (let i in array){
+    multiplicity = multiplicity * array[i]
+  }
+  return multiplicity
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return(num1 ** num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 1){
+    return 1;
+  }
+  return(num * factorial(num -1))
 };
 
 // Do not edit below this line
